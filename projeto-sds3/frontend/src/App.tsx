94a71b05c1173441b6402a4765086a07affc72c1
Robson Vellasques <robson.vellasques@gmail.com>
2021-05-05 21:50:15 -0300

    Basic static components

--- projeto-sds3/frontend/src/App.tsx
+++ projeto-sds3/frontend/src/App.tsx
@@ -1,8 +1,18 @@
+import NavBar from 'components/NavBar/index_lambda';
+import Footer from 'components/Footer/index';
+import DataTable from 'components/DataTable';
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Olá mundo!</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary">Olá mundo!</h1>
+
+        <DataTable />
+      </div>
+      <Footer/>
+
+    </>
   );
 }
 
---
+++ projeto-sds3/frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/robmanv" target="_blank" rel="noreferrer">Robson Vellasques</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+      Evento promovido pela escola DevSuperior: <a href="https://instagram.com/robmanv" target="_blank" rel="noreferrer">@robmanv</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
